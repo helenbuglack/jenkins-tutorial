857e67c37a638942f9bc567585b9e783d190b0ca
helenbuglack <a.buhlak@sam-solutions.com>
2020-07-07 13:38:42 +0300

    update pom.xml file

--- src/main/java/MainTutorialJenkins.java
+++ src/main/java/MainTutorialJenkins.java
@@ -11,3 +11,27 @@ public class MainTutorialJenkins {
         }
     }
 }
+
+//pipeline {
+//        agent any
+//
+//        stages {
+//        stage ('Compile Stage') {
+//
+//        steps {
+//        withMaven(maven : 'maven') {
+//        bat 'mvn clean compile'
+//        }
+//        }
+//        }
+//
+//        stage ('Testing Stage') {
+//
+//        steps {
+//        withMaven(maven : 'maven') {
+//        bat 'mvn test'
+//        }
+//        }
+//        }
+//        }
+//        }
